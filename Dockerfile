00e921c3df883f94095cff90ed103407446df219
Ruben Suarez <rubensa@gmail.com>
2020-06-04 12:06:09 +0200

    Docker image with Wine

---
+++ Dockerfile
@@ -0,0 +1,53 @@
+FROM rubensa/ubuntu-tini-x11
+LABEL author="Ruben Suarez <rubensa@gmail.com>"
+
+# Tell docker that all future commands should be run as root
+USER root
+
+# Set root home directory
+ENV HOME=/root
+
+# Suppress the Wine debug messages
+ENV WINEDEBUG -all
+
+# Avoid warnings by switching to noninteractive
+ENV DEBIAN_FRONTEND=noninteractive
+
+# Configure apt and install packages
+RUN apt-get update && apt-get -y upgrade \
+    # 
+    # Install software and needed libraries
+    && apt-get -y install --no-install-recommends software-properties-common 2>&1 \
+    #
+    # Enable 32 bit architecture
+    && dpkg --add-architecture i386 \
+    #
+    # Add Repos
+    #
+    # Wine repo
+    && curl https://dl.winehq.org/wine-builds/winehq.key | apt-key add - \
+    && add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main' \
+    #
+    # faudio backport repo
+    && curl -L https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_10/Release.key | apt-key add - \
+    && add-apt-repository 'deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/ ./' \
+    #
+    # Install software
+    && apt-get update && apt-get -y upgrade && apt-get -y install --install-recommends winehq-stable winetricks \
+    #
+    # Clean up
+    && apt-get autoremove -y \
+    && apt-get clean -y \
+    && rm -rf /var/lib/apt/lists/*
+
+# Switch back to dialog for any ad-hoc use of apt-get
+ENV DEBIAN_FRONTEND=
+
+# Tell docker that all future commands should be run as the non-root user
+USER ${USER_NAME}
+
+# Set user home directory (see: https://github.com/microsoft/vscode-remote-release/issues/852)
+ENV HOME /home/${USER_NAME}
+
+# Set default working directory to user home directory
+WORKDIR ${HOME}
---
+++ run.sh
@@ -0,0 +1,176 @@
+#!/usr/bin/env bash
+
+# Get current user UID
+USER_ID=$(id -u)
+# Get current user main GUID
+GROUP_ID=$(id -g)
+# Built in user name
+USER_NAME=user
+
+prepare_docker_timezone() {
+  # https://www.waysquare.com/how-to-change-docker-timezone/
+  ENV_VARS+=" --env=TZ=$(cat /etc/timezone)"
+}
+
+prepare_docker_user_and_group() {
+  RUNNER+=" --user=${USER_ID}:${GROUP_ID}"
+}
+
+prepare_docker_dbus_host_sharing() {
+  # To access DBus you ned to start a container without an AppArmor profile
+  SECURITY+=" --security-opt apparmor:unconfined"
+  # https://github.com/mviereck/x11docker/wiki/How-to-connect-container-to-DBus-from-host
+  # User DBus
+  MOUNTS+=" --mount type=bind,source=${XDG_RUNTIME_DIR}/bus,target=${XDG_RUNTIME_DIR}/bus"
+  # System DBus
+  MOUNTS+=" --mount type=bind,source=/run/dbus/system_bus_socket,target=/run/dbus/system_bus_socket"
+  # User DBus unix socket
+  # Prevent "gio:" "operation not supported" when running "xdg-open https://rubensa.eu.org"
+  ENV_VARS+=" --env=DBUS_SESSION_BUS_ADDRESS=/dev/null"
+}
+
+prepare_docker_xdg_runtime_dir_host_sharing() {
+  # XDG_RUNTIME_DIR defines the base directory relative to which user-specific non-essential runtime files and other file objects (such as sockets, named pipes, ...) should be stored.
+  MOUNTS+=" --mount type=bind,source=${XDG_RUNTIME_DIR},target=${XDG_RUNTIME_DIR}"
+  # XDG_RUNTIME_DIR
+  ENV_VARS+=" --env=XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR}"
+}
+
+prepare_docker_sound_host_sharing() {
+  # Sound device (ALSA - Advanced Linux Sound Architecture - support)
+  [ -d /dev/snd ] && DEVICES+=" --device /dev/snd"
+  # Pulseaudio unix socket (needs XDG_RUNTIME_DIR support)
+  MOUNTS+=" --mount type=bind,source=${XDG_RUNTIME_DIR}/pulse,target=${XDG_RUNTIME_DIR}/pulse,readonly"
+  # https://github.com/TheBiggerGuy/docker-pulseaudio-example/issues/1
+  ENV_VARS+=" --env=PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native"
+  RUNNER_GROUPS+=" --group-add audio"
+}
+
+prepare_docker_webcam_host_sharing() {
+  # Allow webcam access
+  for device in /dev/video*
+  do
+    if [[ -c $device ]]; then
+      DEVICES+=" --device $device"
+    fi
+  done
+  RUNNER_GROUPS+=" --group-add video"
+}
+
+prepare_docker_gpu_host_sharing() {
+  # GPU support (Direct Rendering Manager)
+  [ -d /dev/dri ] && DEVICES+=" --device /dev/dri"
+  # VGA Arbiter
+  [ -c /dev/vga_arbiter ] && DEVICES+=" --device /dev/vga_arbiter"
+  # Allow nvidia devices access
+  for device in /dev/nvidia*
+  do
+    if [[ -c $device ]]; then
+      DEVICES+=" --device $device"
+    fi
+  done
+}
+
+prepare_docker_printer_host_sharing() {
+  # CUPS (https://github.com/mviereck/x11docker/wiki/CUPS-printer-in-container)
+  MOUNTS+=" --mount type=bind,source=/run/cups/cups.sock,target=/run/cups/cups.sock"
+  ENV_VARS+=" --env CUPS_SERVER=/run/cups/cups.sock"
+}
+
+prepare_docker_ipc_host_sharing() {
+  # Allow shared memory to avoid RAM access failures and rendering glitches due to X extesnion MIT-SHM
+  EXTRA+=" --ipc=host"
+}
+
+prepare_docker_x11_host_sharing() {
+   # X11 Unix-domain socket
+  MOUNTS+=" --mount type=bind,source=/tmp/.X11-unix,target=/tmp/.X11-unix"
+  ENV_VARS+=" --env=DISPLAY=unix${DISPLAY}"
+  # Credentials in cookies used by xauth for authentication of X sessions
+  MOUNTS+=" --mount type=bind,source=${XAUTHORITY},target=${XAUTHORITY}"
+  ENV_VARS+=" --env=XAUTHORITY=${XAUTHORITY}"
+}
+
+prepare_docker_hostname_host_sharing() {
+  # Using host hostname allows gnome-shell windows grouping
+  EXTRA+=" --hostname `hostname`"
+}
+
+prepare_docker_nvidia_drivers_install() {
+  # NVidia propietary drivers are needed on host for this to work
+  if [ `command -v nvidia-smi` ]; then 
+    NVIDIA_VERSION=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader)
+
+    # On run, if you specify NVIDIA_VERSION the nvidia specified drivers version are installed
+    ENV_VARS+=" --env=NVIDIA_VERSION=${NVIDIA_VERSION}"
+  fi
+}
+
+prepare_docker_fuse_sharing() {
+  # Fuse is needed by AppImage
+  # The kernel requires SYS_ADMIN
+  CAPABILITIES+=" --cap-add SYS_ADMIN"
+  [ -c /dev/fuse ] && DEVICES+=" --device /dev/fuse"
+}
+
+prepare_docker_shared_memory_size() {
+  # https://github.com/SeleniumHQ/docker-selenium/issues/388
+  EXTRA+=" --shm-size=2g"
+}
+
+prepare_docker_in_docker() {
+  # Docker
+  if [ -S /var/run/docker.sock ]; then
+    MOUNTS+=" --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock"
+  fi
+  if [ -f "`command -v docker`" ]; then
+    MOUNTS+=" --mount type=bind,source=`command -v docker`,target=/home/$USER_NAME/.local/bin/docker"
+  fi
+  if [ -f "`command -v docker-compose`" ]; then
+    MOUNTS+=" --mount type=bind,source=`command -v docker-compose`,target=/home/$USER_NAME/.local/bin/docker-compose"
+  fi
+  if [ ! -z "`getent group docker`" ]; then
+    RUNNER_GROUPS+=" --group-add `cut -d: -f3 < <(getent group docker)`"
+  fi
+}
+
+prepare_docker_userdata_volumes() {
+  # User media folders
+  MOUNTS+=" --mount type=bind,source=$HOME/Documents,target=/home/$USER_NAME/Documents"
+  MOUNTS+=" --mount type=bind,source=$HOME/Downloads,target=/home/$USER_NAME/Downloads"
+  MOUNTS+=" --mount type=bind,source=$HOME/Music,target=/home/$USER_NAME/Music"
+  MOUNTS+=" --mount type=bind,source=$HOME/Pictures,target=/home/$USER_NAME/Pictures"
+  MOUNTS+=" --mount type=bind,source=$HOME/Videos,target=/home/$USER_NAME/Videos"
+  # wine config
+  [ -d $HOME/.wine ] || mkdir -p $HOME/.wine
+  MOUNTS+=" --mount type=bind,source=$HOME/.wine,target=/home/$USER_NAME/.wine"
+}
+
+prepare_docker_timezone
+prepare_docker_user_and_group
+prepare_docker_dbus_host_sharing
+prepare_docker_xdg_runtime_dir_host_sharing
+prepare_docker_sound_host_sharing
+prepare_docker_webcam_host_sharing
+prepare_docker_gpu_host_sharing
+prepare_docker_printer_host_sharing
+prepare_docker_ipc_host_sharing
+prepare_docker_x11_host_sharing
+prepare_docker_hostname_host_sharing
+prepare_docker_nvidia_drivers_install
+prepare_docker_fuse_sharing
+prepare_docker_shared_memory_size
+prepare_docker_in_docker
+prepare_docker_userdata_volumes
+
+bash -c "docker run --rm -it \
+  --name ubuntu-tini-wine \
+  ${SECURITY} \
+  ${CAPABILITIES} \
+  ${ENV_VARS} \
+  ${DEVICES} \
+  ${MOUNTS} \
+  ${EXTRA} \
+  ${RUNNER} \
+  ${RUNNER_GROUPS} \
+  rubensa/ubuntu-tini-wine"
